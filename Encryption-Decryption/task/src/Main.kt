b19a7e44e674c2b14c86e7e42b0a9eda1eb41c62
aprilte <apriltexs@gmail.com>
2022-10-08 10:25:48 +0900

    1008::1

--- Encryption-Decryption/task/src/Main.kt
+++ Encryption-Decryption/task/src/Main.kt
@@ -1,17 +1,37 @@
 package tictactoe
 
 fun main() {
-    val orgMessage = "we found a treasure!"
-    println(smpEncrypt(orgMessage))
+    val select = readln()
+    val orgMessage = readln()
+    val encNum = readln().toInt()
+    if (select == "enc") {
+        println(smpEncrypt(orgMessage, encNum))
+    } else {
+        println(smpDecrypt(orgMessage, encNum))
+    }
 }
 
-fun smpEncrypt(org: String): String {
+fun smpEncrypt(org: String, shift: Int): String {
     var retStr: String = ""
-    val diff = 'z'.code - 'a'.code
+    val diff = shift
     org.toCharArray().forEach {
-        if(it in 'a'..'z') {
-            var chr = it.code - diff
-            if(chr < 'a'.code) chr = 'a'.code + ('a'.code - chr)
+        if (true) {
+            var chr = it.code + diff
+//            if ('z'.code < chr) chr = 'a'.code + (chr - ('z'.code + 1))
+            retStr += chr.toChar()
+        } else {
+            retStr += it.toChar()
+        }
+    }
+    return retStr
+}
+
+fun smpDecrypt(msg: String, shift: Int): String {
+    var retStr: String = ""
+    msg.toCharArray().forEach {
+        if (true) {
+            var chr = it.code - shift
+//            if (chr < 'a'.code) chr = 'z'.code - ('a'.code - chr - 1)
             retStr += chr.toChar()
         } else {
             retStr += it.toChar()
--- Encryption-Decryption/task/test/EncryptDecryptTest.java
+++ Encryption-Decryption/task/test/EncryptDecryptTest.java
@@ -9,33 +9,58 @@ public class EncryptDecryptTest extends StageTest<String> {
     @Override
     public List<TestCase<String>> generate() {
         return List.of(
-            new TestCase<String>().setAttach("dv ulfmw z givzhfiv!")
+            new TestCase<String>()
+                .addArguments(
+                    "-mode", "enc",
+                    "-key", "5",
+                    "-data", "Welcome to hyperskill!"
+                )
+                .setAttach("\\jqhtrj%yt%m~ujwxpnqq&"),
+
+            new TestCase<String>()
+                .addArguments(
+                    "-key", "0",
+                    "-mode", "enc",
+                    "-data", "Hello"
+                )
+                .setAttach("Hello"),
+
+            new TestCase<String>()
+                .addArguments(
+                    "-key", "1",
+                    "-data", "012345678",
+                    "-mode", "enc"
+                )
+                .setAttach("123456789"),
+
+            new TestCase<String>()
+                .addArguments(
+                    "-mode", "dec",
+                    "-data", "\\jqhtrj%yt%m~ujwxpnqq&",
+                    "-key", "5"
+                )
+                .setAttach("Welcome to hyperskill!"),
+
+            new TestCase<String>()
+                .addArguments(
+                    "-mode", "dec",
+                    "-key", "0",
+                    "-data", "Hi"
+                )
+                .setAttach("Hi"),
+
+            new TestCase<String>()
+                .addArguments(
+                    "-mode", "dec",
+                    "-key", "1",
+                    "-data", "222233334444"
+                )
+                .setAttach("111122223333")
         );
     }
 
     @Override
     public CheckResult check(String reply, String clue) {
-        reply = reply.trim();
-        clue = clue.trim();
-        boolean isCorrect = reply.trim().equals(clue.trim());
-        if (isCorrect) {
-            return CheckResult.correct();
-        }
-        if (reply.length() != clue.length()) {
-            return CheckResult.wrong(
-                "You should output a line with length " +
-                clue.length() + ". " + "You output a " +
-                "line with length " + reply.length()
-            );
-        }
-        for (int i = 0; i < clue.length(); i++) {
-            if (reply.charAt(i) != clue.charAt(i)) {
-                return CheckResult.wrong(
-                    "Your " + (i+1) + "-th character '" + reply.charAt(i) + "'" +
-                    " is incorrect. The right one is '" + clue.charAt(i) + "'"
-                );
-            }
-        }
-        return CheckResult.correct();
+        return new CheckResult(reply.trim().equals(clue.trim()));
     }
 }
